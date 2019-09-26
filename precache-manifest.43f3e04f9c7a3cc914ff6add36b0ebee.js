19d3598c261c60c7a5f01fd50d3bc1ced93aba6e
Himanshu Sajwan <Himanshu.Sajwan@XEBIAINDIA.LOCAL>
2019-09-27 00:49:09 +0530

    Updates

---
+++ precache-manifest.43f3e04f9c7a3cc914ff6add36b0ebee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9755c02fbd27ab96decc44c609971a7",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "bc4c3b2c8d3878f50871",
+    "url": "/react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "909d1b283236bc06571d",
+    "url": "/react-app/static/css/main.65addd39.chunk.css"
+  },
+  {
+    "revision": "bc4c3b2c8d3878f50871",
+    "url": "/react-app/static/js/2.2e2468ba.chunk.js"
+  },
+  {
+    "revision": "909d1b283236bc06571d",
+    "url": "/react-app/static/js/main.a8492bf4.chunk.js"
+  },
+  {
+    "revision": "b4a087f5abbbe0239832",
+    "url": "/react-app/static/js/runtime~main.02de2757.js"
+  },
+  {
+    "revision": "2404a29efa5e3130a0d1764ca69e2888",
+    "url": "/react-app/static/media/star (2).2404a29e.jpg"
+  },
+  {
+    "revision": "dc53b467c023c8f00da0e6feb82c180f",
+    "url": "/react-app/static/media/star-bg.dc53b467.jpg"
+  },
+  {
+    "revision": "bbf08bd6a53bf9c3dd2015bc43db723d",
+    "url": "/react-app/static/media/star-wars-search.bbf08bd6.jpg"
+  }
+]);
